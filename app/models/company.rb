1c0bcc4a23d99de39a34a36f51adc382247c485e
Cris_Saito <crhisaito@gmail.com>
2021-02-14 22:53:49 -0300

    Configure I18n

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ApplicationRecord
   has_one_attached :logo
   validates :name, :description, :address, :cnpj, :site, :linkedin, presence: true
+  validates :cnpj, uniqueness: true
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Company do
+  context 'validation' do
+    it 'fields cannot be blank' do
+      company = Company.new
+      
+      expect(company.valid?).to eq false
+      expect(company.errors.count).to eq 6
+    end
+    
+    it 'error messages are in portuguese' do
+      company = Company.new
+      company.valid?
+
+      expect(company.errors[:name]).to include('não pode ficar em branco')
+      expect(company.errors[:description]).to include('não pode ficar em branco')
+      expect(company.errors[:address]).to include('não pode ficar em branco')
+      expect(company.errors[:cnpj]).to include('não pode ficar em branco')
+      expect(company.errors[:site]).to include('não pode ficar em branco')
+      expect(company.errors[:linkedin]).to include('não pode ficar em branco') 
+    end
+
+    it 'CNPJ already exists' do
+      Company.create!(name: 'Guaraná', description: 'Guaraná do Brasil', 
+                      address: 'São Paulo, SP', cnpj: '07.526.557/0001-00', 
+                      site: 'www.guarana.com.br', 
+                      linkedin: 'www.linkedin.com/company/guarana')
+      company = Company.new(cnpj: '07.526.557/0001-00')
+
+      company.valid?
+
+      expect(company.errors[:cnpj]).to include('já está em uso')
+    end
+  end
 end
