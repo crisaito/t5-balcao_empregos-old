081ebaeebb65cac96f0b5a205677700c8cfb8f5c
Cris_Saito <crhisaito@gmail.com>
2021-02-23 00:18:01 -0300

    add recruiter model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :recruiters
   root 'home#index'
 
   resources :companies, only: [:index, :show, :new, :create] do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_171519) do
+ActiveRecord::Schema.define(version: 2021_02_22_154609) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -65,7 +65,22 @@ ActiveRecord::Schema.define(version: 2021_02_16_171519) do
     t.index ["company_id"], name: "index_jobs_on_company_id"
   end
 
+  create_table "recruiters", force: :cascade do |t|
+    t.integer "company_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["company_id"], name: "index_recruiters_on_company_id"
+    t.index ["email"], name: "index_recruiters_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_recruiters_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "jobs", "companies"
+  add_foreign_key "recruiters", "companies"
 end
