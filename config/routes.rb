f8ee3df2e36764a1c848baa8d4dfbd74b44bc6e1
Cris_Saito <crhisaito@gmail.com>
2021-02-23 15:30:20 -0300

    redirect when sign in and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :recruiters
   root 'home#index'
+
+  devise_for :recruiters, controllers: { registrations: "registrations" }
+  resources :recruiters, only: [:show]
 
   resources :companies, only: [:index, :show, :new, :create] do
     resources :jobs, only: [:show, :new, :create]
--- spec/features/visitor_login_spec.rb
+++ spec/features/visitor_login_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-feature 'Visitor login' do
-  scenario 'successfully as recruiter' do
+feature 'Visitor' do
+  scenario ' login successfully as recruiter' do
     recruiter = Recruiter.create!(email: 'saito@cocacola.com', password: '123456')
 
     visit root_path
@@ -14,10 +14,40 @@ feature 'Visitor login' do
     end  
 
     expect(page).to have_content recruiter.email
-    expect(page).to have_content 'Cocacola'
+    expect(page).to have_content 'Empresa: Cocacola'
     expect(page).to have_content 'Login efetuado com sucesso.'
     expect(page).to have_content 'Sair'
     expect(page).not_to have_link 'Entrar'
+  end
 
+  scenario 'sign up successfully as recruiter for the first time of company' do
+
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Sign up'
+
+    within('form') do
+      fill_in 'E-mail', with: 'cris@cocacola.com.br' 
+      fill_in 'Senha', with: '123456'
+      fill_in 'Confirme sua senha', with: '123456'
+      click_on 'Sign up'
+    end  
+
+    expect(page).to have_content 'Cadastrar Empresa'
+  end
+
+  scenario 'sign up successfully as recruiter already have company in DB' do
+    recruiter = Recruiter.create!(email: 'saito@cocacola.com', password: '123456')
+
+    visit root_path
+    click_on 'Entrar'
+
+    within('form') do
+      fill_in 'E-mail', with: 'saito@cocacola.com' 
+      fill_in 'Senha', with: '123456'
+      click_on 'Log in'
+    end  
+
+    expect(page).to have_content 'Bem vinda(o) ao painel de controle da Cocacola'
   end
 end
