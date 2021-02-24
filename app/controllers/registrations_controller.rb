d2eff15970f8ac1f93cf7040fa6f78842b5d1a52
Cris_Saito <crhisaito@gmail.com>
2021-02-24 12:47:51 -0300

    redirect recruiter when sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,15 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
-  
+
   def after_sign_up_path_for(resource)
-    new_company_path
+    array = Recruiter.pluck(:company_name)
+    array.pop
+     resource.company_id = 1
+    puts resource.company_id
+    if array.include?(resource.company_name)
+      recruiter_path(resource) 
+    else
+      new_company_path
+    end
   end
 end
--- spec/models/recruiter_spec.rb
+++ spec/models/recruiter_spec.rb
@@ -8,5 +8,15 @@ describe Recruiter do
       expect(recruiter.company_name).to eq 'Cocacola'
 
     end
+
+    it 'company name eq to domain email address' do
+      recruiter1 = Recruiter.create!(email: 'um@dolly.com', password: '123456')
+      recruiter2 = Recruiter.create!(email: 'dois@cocacola.com', password: '123456')
+      recruiter3 = Recruiter.create!(email: 'tres@cocacola.com', password: '123456')
+
+      
+      expect(recruiter3.company_id).to eq 2
+
+    end
   end
 end
